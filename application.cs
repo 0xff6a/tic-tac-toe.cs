36950ed65b547fd71601ed6cfe6c4a029ce59c5f
Jeremy Fox <foxjerem@gmail.com>
2014-09-17 09:51:34 +0100

    Main function added

---
+++ application.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace TicTacToeApplication
+{
+  public class TicTacToeRunner
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Hello Tic Tac Toe World");
+    }
+  }
+}
