3204785e94d82ecd26a6a31244737500b3033e55
Jeremy Fox <foxjerem@gmail.com>
2014-09-17 12:44:25 +0100

    Added turns

--- game.cs
+++ game.cs
@@ -6,11 +6,15 @@ namespace TicTacToe
   {
     private Player challenger;
     private Player opponent;
+    private Player[] players;
+    private Grid board;
 
     public Game(string challengerName, string opponentName)
     {
       challenger = new Player(challengerName, 'X');
       opponent = new Player(opponentName, 'O');
+      players = new Player[2] { challenger, opponent };
+      board = new Grid();
     }
 
     public Player Challenger
@@ -21,6 +25,23 @@ namespace TicTacToe
     public Player Opponent
     {
       get { return opponent; }
+    }
+
+    public Grid Board 
+    {
+      get { return board; }
+    }
+
+    public Player CurrentPlayer()
+    {
+      return players[0];
+    }
+
+    public void ChangeTurn()
+    {
+      Player tempPlayer = players[0];
+      players[0] = players[1];
+      players[1] = tempPlayer;
     }
   }
 }
--- game_tests.cs
+++ game_tests.cs
@@ -16,11 +16,28 @@ namespace TicTacToe
     }
 
     [Test]
-    //Setting up the game
+    //Initializing the game with 2 players and a grid
     public void Start()
     {
       Assert.AreEqual("Me", game.Challenger.Name);
       Assert.AreEqual("EvilMe", game.Opponent.Name);
+      Assert.AreEqual(typeof(Grid), game.Board.GetType());
+    }
+
+    [Test]
+    //The current player
+    public void CurrentPlayer()
+    {
+      Assert.AreEqual(game.Challenger, game.CurrentPlayer());
+    }
+
+    [Test]
+    //changing turns
+    public void ChangeTurn()
+    {
+      game.ChangeTurn();
+
+      Assert.AreEqual(game.Opponent, game.CurrentPlayer());
     }
   }
 }
